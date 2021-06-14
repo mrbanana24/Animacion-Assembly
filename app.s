2bdbb8a13b66ab825e2f79901dd114bfecf41c75
mrbanana24 <franpercivaldi@gmail.com>
2021-06-14 00:11:41 -0300

    Animacion pong naranja

--- app.s
+++ app.s
@@ -19,10 +19,10 @@ loop1:
 	mov x1, SCREEN_WIDTH         // X Size
 loop0:
 	stur w10,[x0]	   // Set color of pixel N
-	add x0,x0,4	   // Next pixel
-	sub x1,x1,1	   // decrement X counter
-	cbnz x1,loop0	   // If not end row jump
-	sub x2,x2,1	   // Decrement Y counter
+	add x0,x0,4	   		// Next pixel
+	sub x1,x1,1	 	 	 // decrement X counter
+	cbnz x1,loop0	  	 // If not end row jump
+	sub x2,x2,1	   		// Decrement Y counter
 	cbnz x2,loop1	   // if not last row, jump
 	mov x0, x20
 
@@ -45,17 +45,7 @@ loop_x5:
 	add x5, x5, 2560
 	sub x14, x14, 1
 	cbnz x14, loop_x5
-
 	cbnz x15, pintar_cuadrado
-
-
-pintar_pong_1:
-	movz w10, 0xFF, lsl 16		// Set color naranja
-	movk w10, 0x6666, lsl 00
-	ldr x5, =0x7D064
-	mov x8, 60     // Tamanio de los cuadrados en Y
-	mov x11, 7     // Tamanio de los cuadrados en X
-	bl square
 
 pintar_pong_2:
 	movz w10, 0x02, lsl 16		// Set color azul
@@ -89,8 +79,6 @@ pintar_marcador_2:
 	mov x8, 45
 	mov x11, 5
 	bl square
-	b InfLoop
-
 
 /* pintar_pelotita:
 	mov x11, 4       // Tamanio de los cuadrados en X
@@ -98,7 +86,42 @@ pintar_marcador_2:
 	bl square
 	b InfLoop */
 
+///////////////////////////      ANIMACION   /////////////////////////////////////////
 
+set_variables:
+	ldr x5, =0x7D064
+	mov x13, 100
+	mov x0, x20
+
+decidir_direccion:
+	sub x13, x13, 1
+	cmp x13, 0
+	bgt animacion_loop_down
+	cmp x13, -200
+	bgt animacion_loop_up
+	b set_variables
+
+animacion_loop_down:
+	add x5, x5, 2560
+	b animacion_pong_1
+
+animacion_loop_up:
+	sub x5, x5, 2560
+	b animacion_pong_1
+
+animacion_pong_1:
+	movz w10, 0xFF, lsl 16		// Set color naranja
+	movk w10, 0x6666, lsl 00
+	mov x8, 60       			// Tamanio de los cuadrados en Y
+	mov x11, 7	   				// Tamanio de los cuadrados en X
+	bl square
+	bl delay
+	movz w10, 0x00, lsl 16		// Set color negro
+	movk w10, 0x0000, lsl 00
+	mov x8, 60       			// Tamanio de los cuadrados en Y
+	mov x11, 7	   				// Tamanio de los cuadrados en X
+	bl square
+	b decidir_direccion
 
 //////////////////////////////////  FUNCIONES  //////////////////////////////////
 
@@ -116,6 +139,17 @@ square_2:
 	cbnz x8, square			// "if(x9 != 0) voy a square para seguir pintando"
 	mov x0, x20
 	br x30
+
+
+delay:
+    movz x14,0x002F, lsl 16
+    movk x14,0xFFFF, lsl 00
+
+loop_delay:
+    sub x14,x14,1
+    cbnz x14,loop_delay
+    br x30
+
 
 	//--------------------------------------------------------------
 	// Infinite Loop
